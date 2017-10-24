9e547f15759c5a0c490dd754b185cb5a9315e53f
Elise <elise_81@yahoo.com>
2017-10-24 04:29:24 -0700

    Add files via upload

---
+++ fizz-buzz.js
@@ -0,0 +1,10 @@
+(function () {
+
+	// multiples of three should print fizz
+	// multiples of five, print buzz
+	// multiples of both three and five, print fizzbuzz
+	for (let i=1; i <= 100; i++) {
+		console.log(i);
+	}
+
+}());
